a0505d9af21ff004422277e7c3b0914df69ceaa4
ReitherNDAWeb <reither@nda-web.com>
2024-02-18 20:15:31 +0100

    first push

---
+++ api/config.php
@@ -0,0 +1,10 @@
+<?php
+
+    define('API_DB_HOST', "mysql");
+	define('API_DB_USER', "db336529_254");
+	define('API_DB_PASS', "MTG4rge!ewc3jbx2uen");
+	define('API_DB_NAME', "db336529_254");
+
+    define('API_STAQQ_EMAIL', "r.reither@da-agentur.at");
+
+?>
